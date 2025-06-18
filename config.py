1880721b42df9e1fc6bba9b4aff15c8f3044b334
rain12335 <105901499+rain12335@users.noreply.github.com>
2025-06-18 19:51:25 +0800

    Update config.py

--- config.py
+++ config.py
@@ -35,16 +35,6 @@ url_blacklist = [
     "epg.pw/stream/",
 ]
 
-announcements = [
-    {
-        "channel": "公告",
-        "entries": [
-            {"name": "直播源均来自网络，不得商用，仅供学习交流使用。", "url": "https://ali-m-l.cztv.com/channels/lantian/channel006/1080p.m3u8", "logo": "https://live1.yangfeiyue.dpdns.org/tv/武术台.png"},
-            {"name": "更新日期", "url": "https://ali-m-l.cztv.com/channels/lantian/channel006/1080p.m3u8", "logo": "https://live1.yangfeiyue.dpdns.org/tv/武术台.png"},
-            {"name": None, "url": "https://live-auth.51kandianshi.com/szgd/csztv4.m3u8", "logo": "https://live1.yangfeiyue.dpdns.org/tv/武术台.png"},
-  ]
-    }
-]
 
 epg_urls = [
     "https://assets.live1dnow.com/epg.xml",
